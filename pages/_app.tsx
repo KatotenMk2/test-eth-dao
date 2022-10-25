0acfe275ccb25d59e97a3d8fdbb325d4815fcd45
KatotenMk2 <cino1031@gmail.com>
2022-10-25 21:54:17 +0900

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from "next/app";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
-import HeadComponent from '../components/head';
+// import HeadComponent from '../components/head';
 
 // アプリケーションが動作するチェーン（Goerli）を取得し、定義します。
 const activeChainId = ChainId.Goerli;
@@ -9,7 +9,7 @@ const activeChainId = ChainId.Goerli;
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <HeadComponent/>
+      {/* <HeadComponent/> */}
       <Component {...pageProps} />
     </ThirdwebProvider>
   );
